84a45cc1885819e2bad6a4cde555ffedf93e6627
Dejiicodes <adewuniayodeji17@gmail.com>
2024-07-30 14:30:03 +0000

    2nd commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
+"""Exercise to practice interview"""
+from itertools import dropwhile
 
 
 def canUnlockAll(boxes):
-    """Unlock array of boxes of keys with indices"""
-    size = len(boxes)
-    checker = {}
-    index = 0
+    """Checks if all boxes can be unlocked"""
 
-    for keys in boxes:
-        if len(keys) == 0 or index == 0:
-            checker[index] = -1  # -1 means box is empty
-        for key in keys:
-                if key < size and key != index:
-                    checker[key] = key
-        if len(checker) == size:
+    keys = {0}
+    range_boxes = range(len(boxes))
+
+    while True:
+        not_found = set()
+
+        for i in range_boxes:
+            if i in keys:
+                for key in dropwhile(lambda k: k in keys, boxes[i]):
+                    keys.add(key)
+            else:
+                not_found.add(i)
+
+        if range_boxes == not_found:
+            return False
+
+        if not not_found:
             return True
-        index += 1
-    return False
+
+        range_boxes = not_found
